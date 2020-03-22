e767072d4f7fcb4a5ab64455b52a276910244559
Lakshya Shankar <lakshyavmc@gmail.com>
2020-03-23 01:41:11 +0530

    Updates

---
+++ precache-manifest.8127ff7f5fa03d34b3ef9d95b37a94c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b721992c5c2a31fa55ebfce525255411",
+    "url": "/MonstersRolodex/index.html"
+  },
+  {
+    "revision": "def49fcfc0a4879a64f8",
+    "url": "/MonstersRolodex/static/css/main.a16efc22.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/MonstersRolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/MonstersRolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "def49fcfc0a4879a64f8",
+    "url": "/MonstersRolodex/static/js/main.4b964b39.chunk.js"
+  },
+  {
+    "revision": "0356c89559ba280291dc",
+    "url": "/MonstersRolodex/static/js/runtime-main.ee30fdb1.js"
+  }
+]);
